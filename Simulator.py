3fc375a05eabba91e55663a48e8af7101ed65bca
pmpm2000 <63394734+pmpm2000@users.noreply.github.com>
2023-12-07 15:46:47 +0100

    asd

--- Simulator.py
+++ Simulator.py
@@ -3,7 +3,7 @@ from numpy import random
 
 
 class Simulator:
-    def __init__(self, distribution='CBR', queue_size=100, packet_limit=1000):
+    def __init__(self, distribution='CBR', queue_size=1000, packet_limit=1000*1000, mi=0.3):
         self.packet_limit = packet_limit
         self.packets_served = 0
         self.clock = 0
@@ -14,10 +14,8 @@ class Simulator:
         self.server_busy = 0
         self.status = 0
         self.spacing_time = 0
-        self.total_lambda_cbr = 0
-        self.number_of_lambda_cbr = 0
-        self.total_lambda_poiss = 0
-        self.number_of_lambda_poiss = 0
+        self.total_lambda = 0
+        self.number_of_lambda= 0
         self.total_mi = 0
         self.number_of_mi = 0
         self.number_of_delays = 0
@@ -26,24 +24,24 @@ class Simulator:
         self.last_index_in_queue = 0
         self.total_delay = 0
         self.L = [6, 3]
-        self.r = [0.3, 0.2]
-        self.avg_lambda_poiss = 0
-        self.avg_lambda_cbr = 0
+        self.r = [0.7, 0.8]
+        self.mi = mi
+        self.avg_lambda = 0
         self.avg_serve_time = 0
         self.avg_queue_time = 0
         self.rho = 0
         self.simulation_percent = 0
         self.avg_server_busy = 0
         self.avg_queue_busy = 0
-        self.poisson_packets = 0
         self.vsi = [0, 0, 0]
-        self.rho_cbr = 0
-        self.rho_poiss = 0
         self.A = [0, self.__generate_event(1), self.__generate_event(2)]  # lista czasow kolejnego zdarzenia dla kazdego typu
         self.A[0] = min(self.A[1], self.A[2]) + self.__generate_event(0)
 
     def __str__(self):
         return f'''
+        Distribution: {self.distribution}
+        Packet sizes: {self.L}
+        r(i): {self.r}
         Clock: {self.clock}
         Status: {self.status}
         Clients in queue: {self.last_index_in_queue}
@@ -54,12 +52,11 @@ class Simulator:
         Average waiting time: {self.avg_queue_time}
         Server busy: {self.avg_server_busy}
         Packets served: {self.packets_served}
-        Average lambda CBR: {self.avg_lambda_cbr}
-        Average lambda Poisson: {self.avg_lambda_poiss}
-        Rho CBR: {self.rho_cbr}
-        Rho Poisson: {self.rho_poiss}
+        Average lambda: {self.avg_lambda}
+        Rho: {self.rho}
         Average serve time (1/mi): {self.avg_serve_time}
-        Poisson packets: {self.poisson_packets}
+        Queue: {self.arrive_time}
+        Events: {self.A}
         ======================'''
 
     def __time_algorithm(self):
@@ -68,28 +65,23 @@ class Simulator:
         return self.A.index(minimum)
 
     def __calculate_vs(self, event_type):
-        if self.distribution == 'expo':
-            L = np.random.exponential(scale=3)
-        else:
-            L = self.L[event_type-1]
+        L = self.L[event_type-1]
         return max(self.spacing_time, self.vsi[event_type]) + L/self.r[event_type-1]
 
     def __generate_event(self, event_type):
         if event_type > 0: # klient przyszedl
-            if event_type == 2:
-                time_between_clients = round(random.exponential(scale=self.r[1]), 2)
-                self.total_lambda_poiss += time_between_clients
-                self.number_of_lambda_poiss += 1
-            elif event_type == 1:
-                time_between_clients = self.r[0]
-                self.total_lambda_cbr += time_between_clients
-                self.number_of_lambda_cbr += 1
+            if self.distribution == 'Poiss':
+                time_between_clients = round(random.exponential(scale=self.r[event_type-1]), 2)
+            elif self.distribution == 'CBR':
+                time_between_clients = self.r[event_type-1]
             else:
                 return
+            self.total_lambda += time_between_clients
+            self.number_of_lambda += 1
 
             return self.clock + time_between_clients  # scale = lamdba (intensywnosc naplywu)
         elif event_type == 0: # klient odszedl
-            serving_time = round(np.random.exponential(scale=0.3), 2)  # scale = 1/mi
+            serving_time = round(np.random.exponential(scale=self.mi), 2)  # scale = 1/mi
             self.total_mi += serving_time
             self.number_of_mi += 1
             return self.clock + serving_time
@@ -106,11 +98,10 @@ class Simulator:
         if event_type > 0:
             vs = self.__calculate_vs(event_type)
             self.vsi[event_type] = vs
-            if event_type == 2:
-                self.poisson_packets += 1
             if self.status == 0: # przychodzi klient, a serwer wolny
                 self.number_of_delays += 1  # liczba opoznien
                 self.status = 1
+                self.A[0] = self.__generate_event(0)
             elif self.status == 1: # przychodzi klient, ale serwer zajety
                 if self.last_index_in_queue < self.queue_size: # kolejka nie jest pelna
                     self.arrive_time.append([vs, (self.clock, event_type)])
@@ -140,24 +131,20 @@ class Simulator:
         while self.packets_served < self.packet_limit:
             event_type = self.__time_algorithm()
             self.__event_algorithm(event_type)
-            if self.clock != 0:
-                self.avg_lambda_cbr = self.total_lambda_cbr / self.number_of_lambda_cbr
-                self.avg_lambda_poiss = self.total_lambda_poiss / self.number_of_lambda_poiss
-                self.avg_serve_time = self.total_mi / self.number_of_mi
-                self.avg_queue_time = self.total_delay / self.number_of_delays
-                self.rho_cbr = self.avg_lambda_cbr * self.avg_serve_time
-                self.rho_poiss = self.avg_lambda_poiss * self.avg_serve_time
-                self.avg_queue_busy = self.queue_busy / self.clock
-                self.avg_server_busy = self.server_busy / self.clock
             last = self.simulation_percent
             self.simulation_percent = round(self.packets_served / self.packet_limit * 100, 0)
             if self.simulation_percent != last:
                 print(f"{self.simulation_percent}%")
-        print(f'Poisson: {random.poisson(lam=self.clock)}')
         print("Simulation completed.")
+        self.avg_lambda = self.total_lambda / self.number_of_lambda
+        self.avg_serve_time = self.total_mi / self.number_of_mi
+        self.avg_queue_time = self.total_delay / self.number_of_delays
+        self.rho = self.avg_lambda * self.avg_serve_time
+        self.avg_queue_busy = self.queue_busy / self.clock
+        self.avg_server_busy = self.server_busy / self.clock
         print(self)
 
 
 if __name__ == "__main__":
-    sim = Simulator(packet_limit=100)
+    sim = Simulator()
     sim.start_simulation()
